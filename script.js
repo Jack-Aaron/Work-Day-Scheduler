080eed459f483c5c19013b5a5cb1da8a73315e6b
Jonathan Alpart <panjialang@gmail.com>
2020-04-29 16:44:05 -0500

    created a fixed time to work with building the app in a static environment

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ $(document).ready(function () {
     console.log(currentTime);
 
     // creates a FIXED time which I can use to build the app
-
+    var fixedTime = moment('1:31:23 PM', 'hh:mm:ss a');
+    console.log(fixedTime._i);
     // creates a FALSE but MOVING time which I can use to test the app
 
 });
