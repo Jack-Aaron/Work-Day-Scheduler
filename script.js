c994193dceb057e45d0e26bbe4abfda5cd8dc370
Jonathan Alpart <panjialang@gmail.com>
2020-04-29 16:50:56 -0500

    got all my times lined up

--- script.js
+++ script.js
@@ -12,8 +12,11 @@ $(document).ready(function () {
     console.log(currentTime);
 
     // creates a FIXED time which I can use to build the app
-    var fixedTime = moment('1:31:23 PM', 'hh:mm:ss a');
+    var fixedTime = moment('1:31 PM', 'hh:mm A');
     console.log(fixedTime._i);
+
     // creates a FALSE but MOVING time which I can use to test the app
+    var falseTime = moment().subtract(6, 'hours').format('hh:mm A')
+    console.log(falseTime);
 
 });
