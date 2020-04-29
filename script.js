43e2ce98de7b68b7a347ff28a2a8742f3c81b161
Jonathan Alpart <panjialang@gmail.com>
2020-04-29 16:39:20 -0500

    grabbing the current time

--- script.js
+++ script.js
@@ -7,4 +7,12 @@ $(document).ready(function () {
     // insert the day in above format into HTML
     day.append(today);
 
+    // takes the current time, which is what the app will run off of
+    var currentTime = moment().format('LT'); 
+    console.log(currentTime);
+
+    // creates a FIXED time which I can use to build the app
+
+    // creates a FALSE but MOVING time which I can use to test the app
+
 });
