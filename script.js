00cffe79f2d3e51a33dceac1a8a062031e96818e
Jonathan Alpart <panjialang@gmail.com>
2020-04-29 16:16:02 -0500

    appends day, month and date to html

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 var day = $("#currentDay");
-var today = moment().format('DD');
+var today = moment().format('dddd, MMMM Do'); 
 console.log(today);
+day.append(today);
 });
