7a793c479cf9f291fe27e031567a600d92669779
Jonathan Alpart <panjialang@gmail.com>
2020-04-29 17:36:30 -0500

    will try new approach dynamically writing time-blocks with JS

--- script.js
+++ script.js
@@ -13,29 +13,41 @@ $(document).ready(function () {
 
     // creates a FIXED time which I can use to build the app
     var fixedTime = moment('13', 'HH');
-    console.log(fixedTime._i);
+    fixedTime = fixedTime._i;
+    console.log(fixedTime);
 
     // creates a FALSE but MOVING time which I can use to test the app
     var falseTime = moment().subtract(6, 'hours').format('HH')
     console.log(falseTime);
+});
 
-    function checkTime(time) {
-        var nineAM = $("#9");
-        var getBlockHours = [];
+  /*  function checkTime(time) {
+     //   var nineAM = $("#9");
+      //  var getBlockHours = [];
         for (let t = 0; t < 9; t++) {
             var checkBlockHour = document.body.children[1].children[t].children[1].id;
             console.log(checkBlockHour);
-            getBlockHours.push(checkBlockHour);
+            console.log(checkBlockHour < time);
+         //   getBlockHours.push(checkBlockHour);
+           if (checkBlockHour < time /* <--CHANGE THIS!  ) {
+              getBlockHours[t].attr("class", "col-10 description past");
+        //    }
+     //   else {getBlockHours[t].attr("class", "col-10 description present")}
+
+
+         //   console.log(checkBlockHour);
+          //  getBlockHours.push(checkBlockHour);
         }
 
         
 
 
-        nineAM.attr("class", "col-10 description present");
+    //    nineAM.attr("class", "col-10 description present");
      //   console.log(getBlockHours);
 
     }
 
-    checkTime(fixedTime._i);
+    checkTime(currentTime);
 
-});
+
+*/
